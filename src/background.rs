c5a7b5b59c88f601bfe641efbbe046d6ae08831a
PurpleMyst <PurpleMyst@users.noreply.github.com>
2020-05-10 22:56:14 +0200

    refactor: use winapi over user32-sys

--- src/background.rs
+++ src/background.rs
@@ -6,6 +6,7 @@ use anyhow::{Context, Result};
 #[cfg(windows)]
 pub fn set(path: &Path) -> Result<()> {
     use std::os::windows::ffi::OsStrExt;
+    use  winapi::um::winuser::SystemParametersInfoW;
 
     let path_utf16 = path
         .as_os_str()
@@ -13,7 +14,7 @@ pub fn set(path: &Path) -> Result<()> {
         .chain(Some(0))
         .collect::<Vec<u16>>();
 
-    let rv = unsafe { user32::SystemParametersInfoW(20, 0, path_utf16.as_ptr() as *mut _, 0) };
+    let rv = unsafe { SystemParametersInfoW(20, 0, path_utf16.as_ptr() as *mut _, 0) };
 
     if rv != 0 {
         Ok(())
